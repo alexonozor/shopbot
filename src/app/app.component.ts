72d888674be39a4c4f012672ce05721b84274806
alexonozor <alexonozor@gmail.com>
2022-08-08 16:32:52 +0100

    fix service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SwPush } from '@angular/service-worker';
 import { environment } from '../environments/environment.prod';
+import { AccountService } from './shared/services/account.service';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +14,19 @@ export class AppComponent implements OnInit {
 
   constructor(
     private swPush: SwPush,
-   
-    // private newsletterService: NewsletterService
+    private accountService: AccountService,
+    private authService: AuthService
   ) {}
 
   ngOnInit(): void {
-    
+   this.subscribeToNotifications()
+
+  //  this.swPush.notificationClicks.subscribe(
+  //   ({action, notification}) => {
+  //     console.log(notification)
+  //       // TODO: Do something in response to notification click.
+  //   });
+
    }
 
 
@@ -28,7 +37,8 @@ export class AppComponent implements OnInit {
         serverPublicKey: environment.publicKey
     })
     .then(sub => {
-      console.log(sub)
+     let adminPush = sub.toJSON()
+      this.accountService.updateStaff(this.authService.getUser._id, {adminPush}).subscribe()
     })
     .catch(err => console.error("Could not subscribe to notifications", err));
 }
